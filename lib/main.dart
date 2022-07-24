9d36c415b4eeff733fb56369877aac9a67410c29
BenouaklilHodhaifa <kh_benouaklil@esi.dz>
2022-07-24 21:31:53 +0100

    Expended widgets nad flex property

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp( MaterialApp(
-  home: Home(),
+void main() => runApp( const MaterialApp(
+  home: const Home(),
 ));
 
 class Home extends StatelessWidget {
@@ -11,25 +11,37 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('first try !'),
+        title: const Text('first try !'),
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Container(
-            color: Colors.cyan,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text('first'),
-                Text('Second'),
-              ],
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: Colors.cyan,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text('first'),
+                  const Text('Second'),
+                ],
+              ),
             ),
           ),
-          ElevatedButton(onPressed: () {print('You pressed the elevated button');}, child: Text('Elevated button')),
-          TextButton(onPressed: () {print('You pressed the text button');}, child: Text('Text button'))
+          Expanded(
+            flex: 2,
+              child: ElevatedButton(
+                  onPressed: () {print('You pressed the elevated button');},
+                  child: const Text('Elevated button'))
+          ),
+          Expanded(
+            flex: 1,
+            child: TextButton(onPressed: () {print('You pressed the text button');},
+                child: const Text('Text button')
+            ),
+          )
         ],
       ),
         );
